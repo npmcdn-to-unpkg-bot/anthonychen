bd1b271a2773ae3034335816a2d555b7cecd9619
Ubuntu <ant@anthonychen.1p1f1vvglcjenji44n4nmcysud.bx.internal.cloudapp.net>
2016-02-13 03:52:22 +0000

    project page filtering, addition of new entry

--- client/js/controllers/ProjectController.js
+++ client/js/controllers/ProjectController.js
@@ -2,13 +2,37 @@ angular
   .module('app')
   .controller('ProjectController', ['$scope', '$state', '$location', 'ProjectEntry', function($scope, $state, $location, $ProjectEntry) {
       $scope.projectEntries = [];
+
       function getProjectEntries() {
 	  $ProjectEntry
-	      .find()
+	      .find({
+		  filter: {
+		      where: {
+			  type: $scope.projectEntryType
+		      }
+		  }
+	      })
 	      .$promise
 	      .then(function(results) {
 		  $scope.projectEntries = results;
 	      });
       }
-      getProjectEntries();
+
+
+      $scope.$watch('projectEntryType', function() {
+	  getProjectEntries();
+      });
+
+      $scope.addProjectEntry = function() {
+	  $scope.newProjectEntry.ctime = new Date();
+	  $scope.newProjectEntry.published = true;
+	  $scope.newProjectEntry.type = $scope.projectEntryType;
+	  $ProjectEntry
+	      .create($scope.newProjectEntry)
+	      .$promise
+	      .then(function(projectEntry) {
+		  $scope.newProjectEntry = '';
+		  getProjectEntries();
+	      });
+      };
   }]);
