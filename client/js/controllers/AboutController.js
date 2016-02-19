39a701fb28c9df4a95ec12565c6dfabb71eb9a0c
Ubuntu <ant@anthonychen.1p1f1vvglcjenji44n4nmcysud.bx.internal.cloudapp.net>
2016-02-19 03:03:35 +0000

    fix resume and fix about page

--- client/js/controllers/AboutController.js
+++ client/js/controllers/AboutController.js
@@ -1,13 +1,10 @@
 angular
   .module('app')
   .controller('AboutController', ['$scope', '$state', '$location', '$anchorScroll', function($scope, $state, $location, $anchorScroll) {
+      $anchorScroll.yOffset = 60;
       $scope.$state = $state;
       $scope.gotoAnchor = function(hash) {
-	  // set the location.hash to the id of
-	  // the element you wish to scroll to.
 	  $location.hash(hash);
-	  
-	  // call $anchorScroll()
 	  $anchorScroll();
       };
 
